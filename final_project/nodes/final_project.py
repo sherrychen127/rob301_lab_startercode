315cb4b1d6bd8e2c7976566ce1760ec8b9df41b8
Adam Heins <mail@adamheins.com>
2021-11-09 12:53:27 -0500

    Attempt to make project code clearer.

--- final_project/nodes/final_project.py
+++ final_project/nodes/final_project.py
@@ -8,28 +8,26 @@ import numpy as np
 
 
 class BayesLoc:
-    def __init__(self, P0, colourCodes, colourMap, transProbBack, transProbForward):
+    def __init__(self, p0, colour_codes, colour_map):
         self.colour_sub = rospy.Subscriber(
             "camera_rgb", UInt32MultiArray, self.colour_callback
         )
         self.line_sub = rospy.Subscriber("line_idx", UInt32, self.line_callback)
         self.cmd_pub = rospy.Publisher("cmd_vel", Twist, queue_size=1)
 
-        self.probability = P0  ## initial state probability is equal for all states
-        self.colourCodes = colourCodes
-        self.colourMap = colourMap
-        self.transProbBack = transProbBack
-        self.transProbForward = transProbForward
-        self.numStates = len(P0)
-        self.statePrediction = np.zeros(np.shape(P0))
+        self.num_states = len(P0)
+        self.colour_codes = colour_codes
+        self.colour_map = colour_map
+        self.probability = P0
+        self.state_prediction = np.zeros(self.num_states)
 
-        self.CurColour = None  ##most recent measured colour
+        self.cur_colour = None  # most recent measured colour
 
     def colour_callback(self, msg):
         """
         callback function that receives the most recent colour measurement from the camera.
         """
-        self.CurColour = np.array(msg.data)  # [r, g, b]
+        self.cur_colour = np.array(msg.data)  # [r, g, b]
 
     def line_callback(self, msg):
         """
@@ -37,74 +35,84 @@ class BayesLoc:
         """
         return
 
-    def waitforcolour(self):
+    def wait_for_colour(self):
         """Loop until a colour is received."""
         rate = rospy.Rate(100)
-        while not rospy.is_shutdown() and self.CurColour is None:
+        while not rospy.is_shutdown() and self.cur_colour is None:
             rate.sleep()
 
-    def measurement_model(self):
-        if self.CurColour is None:
-            self.waitforcolour()
+    def state_model(self, u):
+        """
+        State model: p(x_{k+1} | x_k, u)
+
+        TODO: complete this function
+        """
+
+    def measurement_model(self, x):
+        """
+        Measurement model p(z_k | x_k = colour) - given the pixel intensity,
+        what's the probability that of each possible colour z_k being observed?
+        """
+        if self.cur_colour is None:
+            self.wait_for_colour()
+
         prob = np.zeros(len(colourCodes))
+
         """
-        Measurement model p(z_k | x_k = colour) - given the pixel intensity, what's the probability that  
         TODO: You need to compute the probability of states. You should return a 1x5 np.array
-        Hint: find the euclidean distance between the measured RGB values (self.CurColour) 
-            and the reference RGB values of each color (self.ColourCodes).
+        Hint: find the euclidean distance between the measured RGB values (self.cur_colour)
+            and the reference RGB values of each colour (self.ColourCodes).
         """
+
         return prob
 
-    def statePredict(self, forward):
+    def state_predict(self):
         rospy.loginfo("predicting state")
         """
-        TODO: Complete the state prediction function
+        TODO: Complete the state prediction function: update
+        self.state_prediction with the predicted probability of being at each
+        state (office)
         """
 
-    def stateUpdate(self):
+    def state_update(self):
         rospy.loginfo("updating state")
         """
-        TODO: Complete the state update function
+        TODO: Complete the state update function: update self.probabilities
+        with the probability of being at each state
         """
 
 
 if __name__ == "__main__":
 
+    # This is the known map of offices by colour
     # 0: Green, 1: Purple, 2: Orange, 3: Yellow, 4: Line
     # current map starting at cell #2 and ending at cell #12
-    color_maps = [3, 0, 1, 2, 2, 0, 1, 2, 3, 0, 1]
-    color_codes = [
-        [72, 255, 72],  # green
-        [255, 144, 0],  # orange
+    colour_map = [3, 0, 1, 2, 2, 0, 1, 2, 3, 0, 1]
+
+    # TODO calibrate these RGB values to recognize when you see a colour
+    colour_codes = [
+        [72, 255, 72],    # green
+        [255, 144, 0],    # orange
         [145, 145, 255],  # purple
-        [255, 255, 0],  # yellow
+        [255, 255, 0],    # yellow
         [133, 133, 133],  # line
     ]
 
-    trans_prob_fwd = [0.1, 0.9]
-    trans_prob_back = [0.2, 0.8]
+    # initial probability of being at a given office is uniform
+    p0 = np.ones_like(colour_map) / len(colour_map)
+
+    localizer = BayesLoc(p0, colour_codes, colour_map)
 
     rospy.init_node("final_project")
-    bayesian = BayesLoc(
-        [1.0 / len(color_maps)] * len(color_maps),
-        color_codes,
-        color_maps,
-        trans_prob_back,
-        trans_prob_fwd,
-    )
-    prob = []
     rospy.sleep(0.5)
-    state_count = 0
-
-    prev_state = None
+    rate = rospy.Rate(10)
 
     while not rospy.is_shutdown():
         """
-        TODO: complete this main loop by calling functions from BayesLoc, and adding your own high level and low level planning + control logic
+        TODO: complete this main loop by calling functions from BayesLoc, and
+        adding your own high level and low level planning + control logic
         """
+        rate.sleep()
 
     rospy.loginfo("finished!")
-    rospy.loginfo(bayesian.probability)
-    cmd_publisher = rospy.Publisher("cmd_vel", Twist, queue_size=1)
-    twist = Twist()
-    cmd_publisher.publish(twist)
+    rospy.loginfo(localizer.probability)
